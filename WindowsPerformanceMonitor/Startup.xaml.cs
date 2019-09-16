37938b776bb30270a6c9053f04cec50412ecfd6d
darrennvisak <dvisak@purdue.edu>
2019-09-15 20:18:19 -0400

    Added tabbar

---
+++ WindowsPerformanceMonitor/Startup.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsPerformanceMonitor
+{
+    /// <summary>
+    /// Interaction logic for Startup.xaml
+    /// </summary>
+    public partial class Startup : UserControl
+    {
+        private MainWindow mainWindow = null; // Reference to the MainWindow
+
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        // Get a reference to main windows when it is available.
+        // The Loaded Event is set in the XAML code above.
+        private void OnControlLoaded(object sender, RoutedEventArgs e)
+        {
+            mainWindow = Window.GetWindow(this) as MainWindow;
+        }
+    }
+}
