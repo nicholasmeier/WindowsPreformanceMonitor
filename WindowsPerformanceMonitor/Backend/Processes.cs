1c42ce86453ec49f5fbc14fad45022c912e92c0d
darrennvisak <dvisak@purdue.edu>
2019-09-28 12:22:46 -0400

    Changed how listview binds to process list

--- WindowsPerformanceMonitor/Backend/Processes.cs
+++ WindowsPerformanceMonitor/Backend/Processes.cs
@@ -22,11 +22,11 @@ namespace WindowsPerformanceMonitor.Backend
                 {
                     Name = processes[i].ProcessName,
                     Pid = processes[i].Id,
-                    Cpu = GetCpu(processes[i].Id),
-                    Memory = GetMemory(processes[i].Id),
-                    Gpu = GetGpu(processes[i].Id),
-                    Disk = GetDisk(processes[i].Id),
-                    Network = GetNetwork(processes[i].Id)
+                    Cpu = 0,
+                    Memory = 0,
+                    Gpu = 0,
+                    Disk = 0,
+                    Network = 0
                 };
 
                 processEntries.Add(p);
@@ -35,55 +35,10 @@ namespace WindowsPerformanceMonitor.Backend
             return processEntries;
         }
 
-        private static DateTime lastTime;
-        private static TimeSpan lastTotalProcessorTime;
-        private static DateTime curTime;
-        private static TimeSpan curTotalProcessorTime;
-
         public double GetCpu(int pid)
         {
             return 0;
-
-/*            bool t = true;
-            double CPUUsage = 0;
-
-            // This needs to run async or something because its slow as fuck.
-            Process p = null;
-
-            try
-            {
-                while (t)
-                {
-                    p = Process.GetProcessById(pid);
-                    if (lastTime == null || lastTime == new DateTime())
-                    {
-                        lastTime = DateTime.Now;
-                        lastTotalProcessorTime = p.TotalProcessorTime;
-
-                    }
-                    else
-                    {
-                        curTime = DateTime.Now;
-                        curTotalProcessorTime = p.TotalProcessorTime;
-
-                        CPUUsage = (curTotalProcessorTime.TotalMilliseconds - lastTotalProcessorTime.TotalMilliseconds) / curTime.Subtract(lastTime).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
-
-                        lastTime = curTime;
-                        lastTotalProcessorTime = curTotalProcessorTime;
-                        t = false;
-                    }
-
-                    Thread.Sleep(250);
-                }
-
-                Console.WriteLine(p.ProcessName + CPUUsage * 100 );
-                return CPUUsage * 100;
-            }
-            catch (Exception e)
-            {
-                return 0;
-            }*/
-           
+       
         }
 
         public double GetGpu(int pid)
