d149e580562a70cb59fab1bce7cdd2a45bff8eca
darrennvisak <dvisak@purdue.edu>
2019-09-28 14:08:32 -0400

    Moved updatecpu function

--- WindowsPerformanceMonitor/Backend/Processes.cs
+++ WindowsPerformanceMonitor/Backend/Processes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -35,10 +36,73 @@ namespace WindowsPerformanceMonitor.Backend
             return processEntries;
         }
 
-        public double GetCpu(int pid)
+        public void updateCpu(ObservableCollection<ProcessEntry> procList)
         {
-            return 0;
-       
+            List<DateTime> lastTimes = new List<DateTime>(new DateTime[procList.Count]);
+            List<TimeSpan> lastTotalProcessorTime = new List<TimeSpan>(new TimeSpan[procList.Count]);
+
+            /* Get the current time and total process usage
+                for each process */
+            for (int i = 0; i < procList.Count; i++)
+            {
+                Process p;
+                try
+                {
+                    p = Process.GetProcessById(procList[i].Pid);
+                }
+                catch (ArgumentException)    // Process no longer running
+                {
+                    lastTimes.Insert(i, DateTime.Now);
+                    lastTotalProcessorTime.Insert(i, new TimeSpan(0));
+                    continue;
+                }
+
+                if (lastTimes[i] == null || lastTimes[i] == new DateTime())
+                {
+                    lastTimes.Insert(i, DateTime.Now);
+                    try
+                    {
+                        lastTotalProcessorTime.Insert(i, p.TotalProcessorTime);
+                    }
+                    catch (Exception)       // WIN32 access denied
+                    {
+                        lastTotalProcessorTime.Insert(i, new TimeSpan(0));
+                    }
+                }
+            }
+
+            Thread.Sleep(250);
+
+            /* Get the current time and total process usage
+                for each process, calculate cpu usage
+                based on previous */
+            for (int i = 0; i < procList.Count; i++)
+            {
+                Process p;
+                try
+                {
+                    p = Process.GetProcessById(procList[i].Pid);
+                }
+                catch (ArgumentException)    // Process no longer running
+                {
+                    procList[i].Cpu = -1;
+                    continue;
+                }
+
+                DateTime currTime = DateTime.Now;
+                TimeSpan currTotalProcessorTime;
+                try
+                {
+                    currTotalProcessorTime = p.TotalProcessorTime;
+                }
+                catch (Exception)            // WIN32 access denied.
+                {
+                    currTotalProcessorTime = new TimeSpan(0);
+                }
+
+                double cpuUsage = (currTotalProcessorTime.TotalMilliseconds - lastTotalProcessorTime[i].TotalMilliseconds) / currTime.Subtract(lastTimes[i]).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
+                procList[i].Cpu = cpuUsage * 100;
+            }
         }
     }
 }
