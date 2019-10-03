feef470bdbd31114477b7dae5b42ffbefb5d98fd
darrennvisak <dvisak@purdue.edu>
2019-10-03 18:15:59 -0400

    Misplaced the if statement

--- WindowsPerformanceMonitor/RealTime.xaml.cs
+++ WindowsPerformanceMonitor/RealTime.xaml.cs
@@ -77,17 +77,16 @@ namespace WindowsPerformanceMonitor
         public void UpdateColumnHeaders(ComputerObj comp)
         {
             listView_gridView.Columns[0].Header = $"Process {comp.ProcessList.Count}";
-
-            if (Math.Round(comp.TotalCpu, 2) > 100)
-            {
-                listView_gridView.Columns[1].Header = $"CPU 100%";
-            }
-
             listView_gridView.Columns[1].Header = $"CPU {Math.Round(comp.TotalCpu, 2)}%";
             listView_gridView.Columns[2].Header = $"GPU {Math.Round(comp.TotalGpu, 2)}%";
             listView_gridView.Columns[3].Header = $"Memory {Math.Round(comp.TotalMemory, 2)}%";
             listView_gridView.Columns[4].Header = $"Disk {Math.Round(comp.TotalDisk, 2)}%";
             listView_gridView.Columns[5].Header = $"Network {Math.Round(comp.TotalNetwork, 2)}%";
+
+            if (Math.Round(comp.TotalCpu, 2) > 100)
+            {
+                listView_gridView.Columns[1].Header = $"CPU 100%";
+            }
         }
 
         #endregion
