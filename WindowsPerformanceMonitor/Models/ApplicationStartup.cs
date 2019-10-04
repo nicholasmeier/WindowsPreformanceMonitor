988a9a4078f38c9c78aec1636a68a3f9fdec95ff
chang432 <chang432@purdue.edu>
2019-10-04 02:53:04 -0700

    fixed display name issue

---
+++ WindowsPerformanceMonitor/Models/ApplicationStartup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace WindowsPerformanceMonitor.Models
+{
+    public class ApplicationStartup
+    {
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Status { get; set; }
+    }
+}
