b55d6dc426652ae8d14b1a0ef81bfcb14bbcfe48
Spencer Kline <kline39@purdue.edu>
2019-10-28 15:42:30 -0400

    Show CPU Bus speed

--- WindowsPerformanceMonitor/Models/CPUHardwareDetails.cs
+++ WindowsPerformanceMonitor/Models/CPUHardwareDetails.cs
@@ -9,6 +9,7 @@ namespace WindowsPerformanceMonitor.Models
     class CPUHardwareDetails
     {
         public string _name { get; set; }
+        public double _busSpeedCPU { get; set; }
         public double _clockSpeedCPU { get; set; }
         public int _coresCPU { get; set; }
         public int _logicalCoresCPU { get; set; }
