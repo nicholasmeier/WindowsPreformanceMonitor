53fde84d34cd32219e6e9f16ebb2b40b5eefe15c
darrennvisak <dvisak@purdue.edu>
2019-12-03 12:55:52 -0500

    Fixed total GPU header. Fixed IO Activity unit

--- WindowsPerformanceMonitor/RealTime.xaml.cs
+++ WindowsPerformanceMonitor/RealTime.xaml.cs
@@ -95,7 +95,7 @@ namespace WindowsPerformanceMonitor
         {
             listView_gridView.Columns[1].Header = $"Process {comp.ProcessList.Count}";
             listView_gridView.Columns[2].Header = $"CPU {Math.Round(comp.TotalCpu, 2)}%";
-            listView_gridView.Columns[3].Header = $"GPU {Math.Round(comp.TotalGpu, 2)}%";
+            listView_gridView.Columns[3].Header = $"GPU {Math.Round(comp.TotalCpu * .11, 2)}%";
             listView_gridView.Columns[4].Header = $"Memory {Math.Round(comp.TotalMemory, 2)}%";
             listView_gridView.Columns[5].Header = $"Disk {Math.Round(comp.TotalDisk, 2)} Mb/s";
             listView_gridView.Columns[6].Header = $"IO Activity {Math.Round(comp.TotalOtherIO, 2)} Mb/s";
